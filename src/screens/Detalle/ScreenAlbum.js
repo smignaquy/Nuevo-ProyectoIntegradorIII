d847e68c9920f8f5209a311744c74454aab1d898
BautyRemondino2 <bremondino@udesa.edu.ar>
2023-09-15 19:37:49 -0300

    detalle album terminado

--- src/screens/Detalle/ScreenAlbum.js
+++ src/screens/Detalle/ScreenAlbum.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import TarjetaMusic from "../../components/Tarjeta/TarjetaMusic";
+
+
 
 class ScreenAlbum extends Component{
     constructor(props){
@@ -9,31 +12,67 @@ class ScreenAlbum extends Component{
             dataAlbums: [],
         }
     }
-    render(){
-        let nombreArtista = this.state.dataAlbums.artist ? this.state.dataCancion.artist.name : 'Nombre no disponible';
-        let generosAlbum = this.state.dataAlbums.genres ? this.state.dataCancion.album.title : 'Generos no disponible';
-        return(
-            <></>
-        //     <>  
-        //     <h2 className="artistas">{this.state.dataAlbums.title}</h2>
-        //     <article className="cajita-canciones">
-        //         <img src={`https://e-cdn-images.dzcdn.net/images/cover/${this.state.dataAlbums.cover}/264x264-000000-80-0-0.jpg`} alt="Portada del Album"/>
-        //         {/* informacion */}
-        //         <h2>Artista: </h2><h3>{nombreArtista}</h3>
-        //         <ul>Las canciones pertenecen a los generos: </ul>
-        //         {generosAlbum.data.map((unGenero) => (
-        //             <li>{unGenero.name}</li>
-        //         ))}
 
-        //         <h2>Pertenece al album: </h2><h3></h3>
-        //         <h2>Fecha de publicacion</h2><h3>{this.state.dataCancion.release_date}</h3>
-        //         <div className="detalle">
-        //             <iframe className="detalle" src={this.state.dataCancion.preview} frameBorder="0"/>
-        //         </div>  
-        //     </article>
-        
-        // </>
+    componentDidMount(){
+        //llamo a la api
+        fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${this.state.id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log('data', data)
+            this.setState({
+                dataAlbums: data,
+            })
+
+        })
+        .catch(function(error){
+        console.log('El error es: ' + error);
+        })
+
+        //this.artistaClickeado(id)
+    }
+
+    render(){
+        let nombreArtista = this.state.dataAlbums.artist ? this.state.dataAlbums.artist.name : 'Nombre no disponible';
+        let generoAlbums = this.state.dataAlbums.genres ? this.state.dataAlbums.genres.data : [];
+        let cancionesAlbum = this.state.dataAlbums.tracks ? this.state.dataAlbums.tracks.data : [];
+        console.log(cancionesAlbum);
+        return(
+            <>
+                <h2 className="artistas">{this.state.dataAlbums.title}</h2>
+                <article className="cajita-canciones">
+                    <img className="foto" src={this.state.dataAlbums.cover_medium} alt="Portada del album"/>
+                    <h2>Artista: </h2><h3>{nombreArtista}</h3>
+                    <h2>Fecha de publicacion</h2><h3>{this.state.dataAlbums.release_date}</h3>
+                    <h2>Pertenece al genero: </h2>
+                    <main className="cancionesindex">
+                        {generoAlbums.length === 0 ? (
+                            <img src='./img/loadingGif.gif' alt='Espere a que carge..' className="gif"/>
+                        ) : (
+                            generoAlbums.map((unAlbum) => (
+                                <div className='album'>
+                                    <img src={unAlbum.picture} alt="" />
+                                    <div className="nombresIndex">
+                                        <p className="nombreHome">{unAlbum.name}</p>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </main>
+                    <h2>Algunas de sus canciones son: </h2>
+                    <main className="cancionesindex">
+                        {cancionesAlbum.length === 0 ? (
+                        <img src='./img/loadingGif.gif' alt='Espere a que carge..' className="gif"/>
+                            ) : (
+                            cancionesAlbum.map((unaMusica) => (
+                            // console.log(unaMusica)
+                            <TarjetaMusic data={unaMusica} key={unaMusica.id}/>
+                          ))
+                          )}
+                    </main>
+                    
+                </article>
+            </>
         )
     }
 }
-export default ScreenAlbum; 
+export default ScreenAlbum;
--- src/screens/Detalle/ScreenCancion.js
+++ src/screens/Detalle/ScreenCancion.js
@@ -7,8 +7,6 @@ class ScreenCancion extends Component{
         this.state = {
             id : this.props.match.params.id,
             dataCancion: [],
-            textoBoton: "Agregar a favoritos",
-            favoritos: []
         }
     }
 
@@ -27,71 +25,27 @@ class ScreenCancion extends Component{
         console.log('El error es: ' + error);
         })
 
-        let arrayFavoritos = [];
-        let recuperoStorage = localStorage.getItem('favoritos');
-        
-        if(recuperoStorage !== null){
-            arrayFavoritos = JSON.parse(recuperoStorage);
-
-           if(arrayFavoritos.includes(this.state.dataCancion.id)){
-             this.setState({
-                 textoBoton: 'Quitar de favoritos'
-             })
-           }    
-        }
-    }
-
-    agregarAFavoritos(id){
-        // Agregar un id dentro de array y colocar ese array en localStorage
-        let arrayFavoritos = []
-        let recuperoStorage = localStorage.getItem('favoritos');
-        
-        if(recuperoStorage !== null){
-           arrayFavoritos = JSON.parse(recuperoStorage);   
-        }
-           
-        if(arrayFavoritos.includes(id)){
-            //Si el id está en el array queremos sacar el id.
-            arrayFavoritos = arrayFavoritos.filter( unId => unId !== id);
-
-            this.setState({
-                textoBoton: "Agregar a Favoritos"
-            })
-
-
-        } else {
-            arrayFavoritos.push(id);
-            this.setState({
-                textoBoton: "Quitar de favoritos"
-            })
-        }
-
-        //Subirlo a local storage stringifeado
-        let arrayFavoritosAString = JSON.stringify(arrayFavoritos)
-        localStorage.setItem('favoritos', arrayFavoritosAString)
-
-        console.log('storage', localStorage)
+        //this.artistaClickeado(id)
     }
 
     render(){
         // console.log(this.state.dataCancion.album);
         let nombreArtista = this.state.dataCancion.artist ? this.state.dataCancion.artist.name : 'Nombre no disponible';
-        let albumCancion = this.state.dataCancion.album ? this.state.dataCancion.album.title : 'Nombre no disponible';
+        let albumCancion = this.state.dataCancion.album ? this.state.dataCancion.album : 'Nombre no disponible';
         
         return (
             <>
            
                 <h2 className="artistas">{this.state.dataCancion.title}</h2>
                 <article className="cajita-canciones">
-                    <img src={`https://e-cdn-images.dzcdn.net/images/cover/${this.state.dataCancion.md5_image}/264x264-000000-80-0-0.jpg`} alt="Portada del Album"/>
-                    {/* informacion */}
+                    <img className="foto" src={albumCancion.cover_medium} alt="Portada del album"/>                    {/* informacion */}
+                    <h2>Pertenece al album: </h2>
+                    <h3>{albumCancion.title}</h3>
                     <h2>Artista: </h2><h3>{nombreArtista}</h3>
-                    <h2>Pertenece al album: </h2><h3>{albumCancion}</h3>
                     <h2>Fecha de publicacion</h2><h3>{this.state.dataCancion.release_date}</h3>
                     <div className="detalle">
                         <iframe className="detalle" src={this.state.dataCancion.preview} frameBorder="0"/>
                     </div>
-                    <button id="btn" className="btnVer verIndex" onClick={()=>this.agregarAFavoritos(this.state.dataCancion.id)} type="button">{ this.state.textoBoton }</button>      
                 </article>
             
             </>
@@ -99,5 +53,4 @@ class ScreenCancion extends Component{
     }
     
 }
-export default ScreenCancion; 
-
+export default ScreenCancion;
